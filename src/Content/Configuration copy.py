c1ae892bf8da5cc93cb3a67a838e49eaa0da6c61
ayon_ssp <ayon.ssp@gmail.com>
2023-04-08 14:07:19 +0530

    💡understanding the dataset

---
+++ src/Content/Configuration copy.py
@@ -0,0 +1,10 @@
+# rename this file to Configuration.py and fill in the values
+
+hostname = '<localhost>'
+database = '<db-name>'
+username = '<postgres>'
+pwd = '<password>'
+port_id = 5432
+
+#! databasetype://username:password@hostname:port/database
+dbLink = 'postgresql://'+username+':'+pwd+'@'+hostname+':'+str(port_id)+'/'+database
